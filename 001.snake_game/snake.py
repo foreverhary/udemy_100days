8c5a5f92a57255673ea3b8baa3858a7424761ef7
foreverhary <hongkeon.kim@gmail.com>
2022-05-03 22:45:20 +0900

    snake game class

---
+++ 001.snake_game/snake.py
@@ -0,0 +1,44 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP, DOWN, LEFT, RIGHT = 90, 270, 180, 0
+
+
+class Snake:
+    def __init__(self):
+
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        self.set_heading(UP)
+
+    def down(self):
+        self.set_heading(DOWN)
+
+    def left(self):
+        self.set_heading(LEFT)
+
+    def right(self):
+        self.set_heading(RIGHT)
+
+    def set_heading(self, heading):
+        if abs(self.head.heading() - heading) != 180:
+            self.head.setheading(heading)
